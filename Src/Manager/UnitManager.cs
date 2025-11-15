42cadaf60ae5fb4bdfc2e5ebaf3820e0d1db9621
RandallSungcang <randallsungcang3@gmail.com>
2025-11-15 10:49:25 -0600

    Created new unit manager

---
+++ Src/Manager/UnitManager.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+/*
+* For now this manager will also handle the party, if this manager does too much.
+* We can refactor it later and make a separate manager for the party
+*/
+public partial class UnitManager : Node
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
