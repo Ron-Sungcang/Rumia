ee68f24d27ba4e7797a5bab0c5d9fe1611b1033f
RandallSungcang <randallsungcang3@gmail.com>
2026-01-03 03:39:16 -0600

    Units clickable but cursor still doesnt change

--- Src/Units/PartyUnits/PartyUnit.cs
+++ Src/Units/PartyUnits/PartyUnit.cs
@@ -3,13 +3,44 @@ using System;
 
 public partial class PartyUnit : Units, IPartyUnit
 {
+	[Export] private Area2D area2D;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		if(area2D == null)
+		{
+			GD.Print("PartyUnit, unset collision box");
+			return;
+		}
+		
+		area2D.MouseEntered += OnMouseEntered;
+		area2D.MouseExited += OnMouseExited;
+		
+		area2D.InputEvent += OnAreaInputEvent;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+	
+	private void OnAreaInputEvent(Node viewport, InputEvent @event, long shapeIdx)
+	{
+		if (@event is InputEventMouseButton mouse && mouse.ButtonIndex == MouseButton.Left && mouse.Pressed)
+		{
+			GD.Print("CLICKED ", Name);
+		}
+	}
+	
+	public void OnMouseEntered()
+	{
+		GD.Print("MouseEntered");
+		Input.SetDefaultCursorShape(Input.CursorShape.PointingHand);
+	}
+	
+	public void OnMouseExited()
+	{
+		GD.Print("MouseExited");
+		Input.SetDefaultCursorShape(Input.CursorShape.Arrow);
 	}
 }
