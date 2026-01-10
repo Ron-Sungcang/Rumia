b8a2ead7b14d40f44e2b60c834f72e6e87714689
Ron-Sungcang <sungcangjoseph@gmail.com>
2026-01-10 17:43:52 -0600

    Eventbus handler signals the card aimselector now

--- Src/UI/CardTargetSelector.cs
+++ Src/UI/CardTargetSelector.cs
@@ -4,5 +4,23 @@ using System;
 public partial class CardTargetSelector : Node2D
 {
 	private int ARC_POINTS = 8;
-	
+	private Area2D area_2d;
+	private Line2D line_2d;
+	private Events events;
+
+	public override void _Ready()
+	{
+		if(area_2d == null)
+			area_2d = GetNode<Area2D>("Area2D");
+		if(line_2d == null)
+			line_2d = GetNode<Line2D>("CardArc");
+		
+		events = GetNode<Events>("/root/Events");
+		events.Connect(Events.SignalName.CardAimStarted, Callable.From<Card>(OnCardAimStarted));
+	}
+
+	public void OnCardAimStarted(Card card)
+	{
+		GD.Print("Card Aim started with card:" + card.Name);
+	}
 }
